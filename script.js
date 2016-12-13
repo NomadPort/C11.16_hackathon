ad9524e26478e8a617f6d0a605f1a68983b5555a
Tammyslau29 <Tammyslau29@gmail.com>
2016-12-12 19:53:27 -0800

    Embedding Chosen Videos Working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function to grab Youtube channel
 function get_channel(){
     var youtube_username = $("#youtube_clip").val();
     var channel_data;
@@ -19,6 +20,7 @@ function get_channel(){
             get_video(upload_info);
         })
 }
+//function to grab playlist from channel
 function get_video(video_address){
     var grab_song;
     $.ajax({
@@ -38,10 +40,14 @@ function get_video(video_address){
             for(var i = 0; i < song_array.length; i++) {
                 var new_object = {};
                 new_object.description = playlist_data.items[i].snippet.description;
-                new_object.title = $("<h4>").text(playlist_data.items[i].snippet.title).addClass("col-md-8");
-                new_object.thumbnail = $("<img>").attr("src", playlist_data.items[i].snippet.thumbnails.default.url).addClass("col-md-6 col-md-offset-3").click();
+                new_object.title = $("<div>")
+                    .append($("<label>").text(playlist_data.items[i].snippet.title))
+                    .append($("<input>")
+                        .attr("type","checkbox")
+                        .attr("value",playlist_data.items[i].snippet.resourceId.videoId))
+                    .addClass("video_title");
+                new_object.thumbnail = $("<img>").attr("src", playlist_data.items[i].snippet.thumbnails.default.url).addClass("col-md-6 col-md-offset-3");
                 new_object.video_id = $("<iframe>").attr('width', 460).attr("height", 305).attr("src", "https://www.youtube.com/embed/" + playlist_data.items[i].snippet.resourceId.videoId).attr("frameborder", 0).addClass("col-md-12");
-                // playlist_data.items[i].snippet.resourceId.videoId
                 videos_array.push(new_object);
                 $(".video_display").append(videos_array[i].title, videos_array[i].video_id)
             }
@@ -54,4 +60,25 @@ function expand(){
         $('#info').hide()
     }
 console.log('ive been clicked!')
+}
+function clear_youtube_form(){
+    $("#youtube_clip").val("");
+    $(".video_display").html("");
+}
+//function to append chosen videos to create profile form
+function upload_video() {
+    var checkboxes = $(".video_title input");
+    for (var i = 0; i < checkboxes.length; i++){
+        console.log(checkboxes[i])
+        if($(checkboxes[i]).is(':checked')){
+            $(".youtube_clip_area").append(
+                $("<iframe>")
+                    .attr('width', 460)
+                    .attr("height", 305)
+                    .attr("src", "https://www.youtube.com/embed/" + checkboxes[i].value)
+                    .attr("frameborder", 0)
+                    .addClass("col-md-12")
+            );
+        }
+    }
 }
